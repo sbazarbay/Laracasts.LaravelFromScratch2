0c1e4b749ea2bb059ce02a86992f8d9224cbed82
Cheena <sh.bazarbay@gmail.com>
2020-09-06 14:00:46 +0600

    Completed Episode 51

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 use Illuminate\Support\Facades\Gate;
+use App\User;
 
 class AuthServiceProvider extends ServiceProvider
 {
@@ -25,6 +26,10 @@ class AuthServiceProvider extends ServiceProvider
     {
         $this->registerPolicies();
 
-        //
+        Gate::before(function (User $user) {
+            if($user->id == 4) {
+                return true;
+            }
+        });
     }
 }
